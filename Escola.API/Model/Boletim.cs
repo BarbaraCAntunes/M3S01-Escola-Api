32708ef4e757b6aef6b99aac6a2c526f869a2ddb
Barbara Antunes <barbaracantunes@gmail.com>
2023-09-17 11:51:29 -0300

    Add entidade Boletim, NotasMateria e Materia

---
+++ Escola.API/Model/Boletim.cs
@@ -0,0 +1,10 @@
+﻿namespace Escola.API.Model
+{
+    public class Boletim
+    {
+        public int Id { get; set; }
+        public int AlunoId { get; set; }
+        public Aluno Aluno { get; set; }
+        public List<NotasMateria> NotasMaterias { get; set; }
+    }
+}
