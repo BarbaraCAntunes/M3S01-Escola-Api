beef9542e4567f33d7b98abea1121311b93935cb
Barbara Antunes <barbaracantunes@gmail.com>
2023-09-17 12:38:20 -0300

    post boletim

--- Escola.API/Controllers/BoletimController.cs
+++ Escola.API/Controllers/BoletimController.cs
@@ -1,4 +1,5 @@
 ﻿using Escola.API.DataBase;
+using Escola.API.DTO;
 using Escola.API.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -45,5 +46,25 @@ namespace Escola.API.Controllers
             return boletins;
         }
 
+        [HttpPost]
+        public async Task<ActionResult<Boletim>> PostBoletim(BoletimDTO boletimDTO)
+        {
+            var aluno = await _context.Alunos.FindAsync(boletimDTO.AlunoId);
+
+            if (aluno == null)
+            {
+                return BadRequest("O aluno associado não foi encontrado.");
+            }
+            var boletim = new Boletim
+            {
+                AlunoId = boletimDTO.AlunoId,
+            };
+
+            _context.Boletins.Add(boletim);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBoletim), new { id = boletim.Id }, boletim);
+        }
     }
+
 }
---
+++ Escola.API/DTO/BoletimDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Escola.API.DTO
+{
+    public class BoletimDTO
+    {
+        public int Id { get; set; }
+        public int MateriaId { get; set; }
+        public decimal Nota { get; set; }
+
+        public int AlunoId { get; set; }
+    }
+}
